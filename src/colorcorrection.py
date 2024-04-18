5d9450d456657a741da4f9e961fe1e8eb2488851
Alexandria Anderson <77866068+Alexandria-Anderson@users.noreply.github.com>
2024-04-17 20:28:10 -0400

    Updated

--- src/colorcorrection.py
+++ src/colorcorrection.py
@@ -1,6 +1,9 @@
 from skimage import io
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
+
+Image.MAX_IMAGE_PIXELS = None
 
 MXYZ_cam_dcraw = np.array([
     8139,-2171,-663,-8747,16541,2295,-1925,2008,8093
@@ -19,16 +22,43 @@ MsRGB_cam = np.dot(MXYZ_cam, MsRGB_XYZ)
 MsRGB_cam_normalized = MsRGB_cam / np.sum(MsRGB_cam, axis=1, keepdims=True)
 
 def color_space_correction(image):
+    # Reshape the image to a 2D array where each row represents a pixel
     reshaped_image = np.reshape(image, (-1, 3))
+    
+    # Apply color space correction to all pixels at once
     corrected_pixels = np.dot(reshaped_image, np.linalg.inv(MsRGB_cam_normalized).T)
+    
+    # Reshape the corrected pixels back to the original image shape
     corrected_image = np.reshape(corrected_pixels, image.shape)
     
     return corrected_image
 
-image_path = "data/baby.jpeg"
+# Load the image
+image_path = "image path"
 image = io.imread(image_path)
 
+
+
+
+# Apply color space correction
 corrected_image = color_space_correction(image)
 
-plt.imshow(corrected_image)
+
+# brightness and gamma
+
+
+def adjust_brightness(image, brightness):
+    adjusted_image = np.clip(image + brightness, 0, 255).astype(np.uint8)
+    return adjusted_image
+
+
+def gamma_encoding(image, gamma):
+    encoded_image = np.clip(image ** gamma, 0, 255).astype(np.uint8)
+    return encoded_image
+
+image = adjust_brightness(corrected_image, 100)
+image = gamma_encoding(image, 100)
+io.imsave('image path', image)
+
+plt.imshow(image)
 plt.show()
